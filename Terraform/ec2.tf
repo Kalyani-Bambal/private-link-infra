8fb4e8a249daf579cb0b40991fc7b8f4774166b3
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2025-11-23 18:06:49 +0000

    we update security group

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "ec2-instance" {
-  ami           = "ami-0a71e3eb8b23101ed"
-  instance_type = "t3.micro"
-  key_name      = aws_key_pair.deployer-key.key_name
+  ami                    = "ami-0a71e3eb8b23101ed"
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.deployer-key.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
     Name = "MyEC2Instance"
@@ -14,3 +15,39 @@ resource "aws_key_pair" "deployer-key" {
   key_name   = "private-link-infra-key"
   public_key = file("/home/ashu/Kalyani/Git/private-link-infra/Terraform/ashu.pub")
 }
+
+#create security group
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Security group for EC2 instance"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
