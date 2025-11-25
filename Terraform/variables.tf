72709c74b5c321c7c1f09f5e335fe96179906da5
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2025-11-25 06:59:42 +0000

    file chnage

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -4,7 +4,7 @@ variable "public_key" {
   sensitive   = true
 }
 
-varibale "instance_type" {
+variable "instance_type" {
   description = "Type of EC2 instance"
   type        = string
   default     = "t3.micro"
